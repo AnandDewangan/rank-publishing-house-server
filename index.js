b1c1b8e1fb5edd5f6c2e3ee79d47e5bdd5d7b88f
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-16 17:19:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -9,33 +8,37 @@ import bookRoutes from "./routes/bookRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
 
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads")); 
+app.use("/uploads", express.static("uploads"));
 
-// admin routes 
+// Admin routes 
 app.use("/api/admin", adminRoutes);
 
-// author routes 
-app.use("/api/authors", authorRoutes); 
+// Author routes 
+app.use("/api/authors", authorRoutes);
 
-// book routes 
+// Book routes 
 app.use("/api/books", bookRoutes);
 
-// order routes 
+// Order routes 
 app.use("/api/orders", orderRoutes);
 
-// transaction routes 
+// Transaction routes 
 app.use("/api/transactions", transactionRoutes);
 
+// MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => console.log("Server running on port 5000"));
   })
   .catch((err) => console.log(err));
+
+// Export the Express app for Vercel to use as a serverless function
+export default (req, res) => {
+  app(req, res);  // This will allow Vercel to handle the request/response
+};
