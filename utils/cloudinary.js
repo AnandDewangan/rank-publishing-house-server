37926cef5a60b18df56ce4a83cd3adc80b764280
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-22 17:31:19 +0530

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,11 +1,18 @@
 import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
-dotenv.config();
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "book_covers",
+    allowed_formats: ["jpg", "jpeg", "png"],
+  },
+});
+
+export { cloudinary, storage };
