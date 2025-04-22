4c9060c6a57f62542211f86fcc0519b3942db387
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-22 17:32:11 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,18 +1,9 @@
 import express from 'express';
 import multer from 'multer';
+import { storage } from "../utils/cloudinary.js";
 import { deleteBook, addBook, getBooks, updateBook, getBookStats, getAuthorBookStats } from '../controllers/bookController.js';
 
 const router = express.Router();
-
-// Multer storage setup for handling file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
 
 const upload = multer({ storage });
 
