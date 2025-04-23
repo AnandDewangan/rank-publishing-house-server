441f0ab75aadaa5bfc19013d1236551a81bba06e
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-23 14:48:00 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { getAuthors, getAuthorDetails, updateAuthor, upload, adminLogin } from "../controllers/adminController.js"; 
+import { getAuthors, getAuthorDetails, updateAuthor, adminLogin } from "../controllers/adminController.js"; 
 import { verifyToken } from "../middleware/verifyToken.js";
 import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
 const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + file.originalname;
+    cb(null, uniqueSuffix);
+  }
+});
+const upload = multer({ storage });
 
 router.post("/login", adminLogin); 
 
