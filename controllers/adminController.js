2566499960c0888cce098bbea732cf9967f41e81
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-23 14:48:27 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,23 +1,10 @@
 import User from "../models/User.js";
-import multer from "multer";
 import path from "path";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 import { verifyToken } from "../middleware/verifyToken.js";
 import { verifyAdmin } from "../middleware/verifyAdmin.js";
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = `${Date.now()}-${file.originalname}`;
-    cb(null, uniqueName);
-  },
-});
-
-export const upload = multer({ storage }); 
 
 export const adminLogin = (req, res) => {
   const { email, password } = req.body;
