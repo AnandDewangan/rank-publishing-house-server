fd860e8d2e3e3fc7e50d20c5b23d3e80ce89adfd
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-04-23 14:17:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 // Routes
 app.use("/api/admin", adminRoutes);
