6dbb8db8d8055a81941b7067ecd8a5effda23912
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2025-06-06 17:14:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import feedbackRoutes from './routes/feedbackRoutes.js';
 import articleRoutes from "./routes/articleRoutes.js";
 import entryRoutes from './routes/entryRoutes.js';
 import paymentRoutes from "./routes/payment.js";
+import authRoutes from './routes/authRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
 
 // Load environment variables
@@ -55,6 +56,7 @@ app.use("/api/articles", articleRoutes);
 app.use('/api/entry', entryRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use('/api/books', reviewRoutes);
+app.use("/api/auth", authRoutes);
 
 // MongoDB connection
 let isConnected = false;
